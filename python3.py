a3d48ff98294e7c88d88aed32a1501b6f0ec58ea
Steven Albert <steven.a.540@hotmail.com>
2017-09-01 12:12:25 +0700

    add python example practice for input, output, and numerical input & output

---
+++ python3.py
@@ -0,0 +1,3 @@
+name = "Sophus"
+print(name, "how are you doing?")
+print("I hope that,", name, "is feeling well today.")
---
+++ python4.py
@@ -0,0 +1,4 @@
+base = float(input("Please enter a number: "))
+exp = float(input("Please enter an exponent: "))
+answer = base ** exp
+print("%1.2f^%1.2f = %1.4f"%(base, exp, answer))
