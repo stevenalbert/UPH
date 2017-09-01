43090092bb6a7bd00e6761e707c58a7fc828c4c2
Steven Albert <steven.a.540@hotmail.com>
2017-09-01 12:24:59 +0700

    add python example practice for Objects

---
+++ python12.py
@@ -0,0 +1,16 @@
+#need X server to run GUI
+#install python3-tk
+
+import turtle
+
+t = turtle.Turtle()
+screen = t.getscreen()
+t.forward(25)
+t.left(90)
+t.forward(25)
+t.left(90)
+t.forward(25)
+t.left(90)
+t.forward(25)
+screen.exitonclick()
+
---
+++ python13.py
@@ -0,0 +1,17 @@
+#need X server to run GUI
+#install python3-tk
+
+from turtle import *
+
+t = Turtle()
+screen = t.getscreen()
+t.forward(25)
+t.left(72.5)
+t.forward(25)
+t.left(72.5)
+t.forward(25)
+t.left(72.5)
+t.forward(25)
+t.left(72.5)
+t.forward(25)
+screen.exitonclick()
